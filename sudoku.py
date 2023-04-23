886b282d7cc3ec24e0423db290772b462ae73211
Jiri Cernohorsky <jiri.cernohorsky3@gmail.com>
2023-04-23 22:42:58 +0200

    rename generate_sudoku to sudoku_solver

--- sudoku.py
+++ sudoku.py
@@ -7,28 +7,24 @@ def print_dictionary(board: dict):
 
 
 def validate_sudoku(board: dict):
-    for column_id in range(9):
-        if number == board[input_row_id][column_id]:
-            if mode == 0:
-                return False
-            else:
-                return [input_row_id, column_id]
-    for row_id in range(9):
-        if number == board[row_id][input_column_id]:
-            if mode == 0:
-                return False
-            else:
-                return [row_id, input_column_id]
-    box_start_row = input_row_id // 3 * 3
-    box_start_col = input_column_id // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if board[i + box_start_row][j + box_start_col] == number:
-                if mode == 0:
+    for current_row_id in range(9):
+        for current_column_id in range(9):
+            number = board[current_row_id][current_column_id]
+            for column_id in range(9):
+                if number == board[current_row_id][column_id]:
                     return False
-                else:
-                    return [i + box_start_row, j + box_start_col]
-    return True
+            for row_id in range(9):
+                if number == board[row_id][current_column_id]:
+                    return False
+            box_start_row = current_row_id // 3 * 3
+            box_start_col = current_column_id // 3 * 3
+            for i in range(3):
+                for j in range(3):
+                    if board[i + box_start_row][j + box_start_col] == number:
+                        return False
+            return True
+
+
 def is_valid_number(number, input_column_id, input_row_id, board: dict, mode: int):
     for column_id in range(9):
         if number == board[input_row_id][column_id]:
@@ -54,26 +50,25 @@ def is_valid_number(number, input_column_id, input_row_id, board: dict, mode: in
     return True
 
 
-def generate_sudoku(board: dict):
-    index = 0
-    while index < 81:
+def sudoku_solver(board: dict, numbers_solved: int):
+    while numbers_solved < 81:
         row_id = random.randint(0, 8)
         column_id = random.randint(0, 8)
         if board[row_id][column_id] == " ":
             generated_number = random.randint(1, 9)
             if is_valid_number(generated_number, column_id, row_id, board, 0):
                 board[row_id][column_id] = generated_number
-                index += 1
+                numbers_solved += 1
             else:
                 for z in range(1, 10):
                     if is_valid_number(z, column_id, row_id, board, 0):
                         board[row_id][column_id] = z
-                        index += 1
+                        numbers_solved += 1
                         break
                 else:
                     coordinates = is_valid_number(generated_number, column_id, row_id, board, 1)
                     board[int(coordinates[0])][int(coordinates[1])] = " "
-                    index -= 1
+                    numbers_solved -= 1
     return board
 
 
@@ -83,6 +78,5 @@ for rowIndex in range(9):
     for columnIndex in range(9):
         rows.append(" ")
     sudoku[rowIndex] = rows
-
-sudoku = generate_sudoku(sudoku)
+sudoku = sudoku_solver(sudoku, 0)
 print_dictionary(sudoku)
